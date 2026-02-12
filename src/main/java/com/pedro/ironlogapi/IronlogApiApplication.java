887562442f449cfee554c77e4bab83e2670fa18b
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-12 09:21:39 -0300

    Initial commit

---
+++ src/main/java/com/pedro/ironlogapi/IronlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.ironlogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronlogApiApplication.class, args);
+	}
+
+}
