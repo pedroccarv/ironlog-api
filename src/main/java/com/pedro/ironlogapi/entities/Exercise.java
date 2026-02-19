87032d433b046229ed27b338ad6dd31016aaa7c5
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-19 09:16:13 -0300

    feat: add Exercise entity

---
+++ src/main/java/com/pedro/ironlogapi/entities/Exercise.java
@@ -0,0 +1,24 @@
+package com.pedro.ironlogapi.entities;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_exercise")
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String muscleGroup;
+
+}
