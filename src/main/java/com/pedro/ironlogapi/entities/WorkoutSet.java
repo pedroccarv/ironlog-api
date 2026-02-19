845a9b5c60654c4b7d32a95f08d878d1e6bc5bb6
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-19 09:31:35 -0300

    feat: add WorkoutSet entity and finalize database schema

---
+++ src/main/java/com/pedro/ironlogapi/entities/WorkoutSet.java
@@ -0,0 +1,29 @@
+package com.pedro.ironlogapi.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_workout_sets")
+public class WorkoutSet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer reps;
+    private Double weight;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id")
+    private Workout workout;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id")
+    private Exercise exercise;
+
+}
