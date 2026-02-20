c98accb237d18c24bfe88dd545fed945d8aa5edf
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-20 10:37:05 -0300

    feat: implement full CRUD operations for Exercise entity

--- src/main/java/com/pedro/ironlogapi/resource/ExerciseResource.java
+++ src/main/java/com/pedro/ironlogapi/resource/ExerciseResource.java
@@ -5,7 +5,9 @@ import com.pedro.ironlogapi.service.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,6 +27,25 @@ public class ExerciseResource {
     public ResponseEntity<Exercise> getById(@PathVariable Long id) {
         Exercise exercise = exerciseService.findById(id);
         return ResponseEntity.ok().body(exercise);
+    }
+
+    @PostMapping
+    public ResponseEntity<Exercise> create(@RequestBody Exercise obj) {
+        obj = exerciseService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        exerciseService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Exercise> update(@PathVariable Long id, @RequestBody Exercise obj) {
+        obj = exerciseService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
--- src/main/java/com/pedro/ironlogapi/service/ExerciseService.java
+++ src/main/java/com/pedro/ironlogapi/service/ExerciseService.java
@@ -2,7 +2,11 @@ package com.pedro.ironlogapi.service;
 
 import com.pedro.ironlogapi.entities.Exercise;
 import com.pedro.ironlogapi.repositories.ExerciseRepository;
+import com.pedro.ironlogapi.service.exceptions.DatabaseException;
+import com.pedro.ironlogapi.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +25,36 @@ public class ExerciseService {
     public Exercise findById(Long id) {
         Optional<Exercise> exercise = exerciseRepository.findById(id);
         return exercise.orElseThrow(() -> new RuntimeException("Exercise not found"));
+    }
+
+    public Exercise insert(Exercise obj) {
+        return exerciseRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+        if (!exerciseRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Exercise not found");
+        }
+        try {
+            exerciseRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation: cannot delete with existing workout sets");
+        }
+    }
+
+    public void updateData(Exercise entity, Exercise obj){
+        entity.setName(obj.getName());
+        entity.setId(obj.getId());
+    }
+
+    public Exercise update(Long id, Exercise obj) {
+        try {
+            Exercise entity = exerciseRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return exerciseRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
 }
