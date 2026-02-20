4351e5004bbf6e391b92604138d4aa77c719231c
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-20 11:10:39 -0300

    feat: implement full CRUD operations for WorkoutSet entity

--- src/main/java/com/pedro/ironlogapi/resource/WorkoutSetResource.java
+++ src/main/java/com/pedro/ironlogapi/resource/WorkoutSetResource.java
@@ -4,11 +4,10 @@ import com.pedro.ironlogapi.entities.WorkoutSet;
 import com.pedro.ironlogapi.service.WorkoutSetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +27,25 @@ public class WorkoutSetResource {
     public ResponseEntity<WorkoutSet> getWorkoutSetById(@PathVariable Long id) {
         WorkoutSet workoutSet = workoutSetService.getWorkoutSetById(id);
         return ResponseEntity.ok().body(workoutSet);
+    }
+
+    @PostMapping
+    public ResponseEntity<WorkoutSet> createWorkoutSet(@RequestBody WorkoutSet workoutSet) {
+        workoutSet = workoutSetService.createWorkoutSet(workoutSet);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(workoutSet.getId()).toUri();
+        return ResponseEntity.created(uri).body(workoutSet);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteWorkoutSet(@PathVariable Long id) {
+        workoutSetService.deleteWorkoutSetById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<WorkoutSet> updateWorkoutSet(@RequestBody WorkoutSet obj, @PathVariable Long id) {
+        obj = workoutSetService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
--- src/main/java/com/pedro/ironlogapi/service/WorkoutSetService.java
+++ src/main/java/com/pedro/ironlogapi/service/WorkoutSetService.java
@@ -2,7 +2,11 @@ package com.pedro.ironlogapi.service;
 
 import com.pedro.ironlogapi.entities.WorkoutSet;
 import com.pedro.ironlogapi.repositories.WorkoutSetRepository;
+import com.pedro.ironlogapi.service.exceptions.DatabaseException;
+import com.pedro.ironlogapi.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +24,40 @@ public class WorkoutSetService {
 
     public WorkoutSet getWorkoutSetById(Long id) {
         Optional<WorkoutSet> workoutSet = workoutSetRepository.findById(id);
-        return workoutSet.orElseThrow(() -> new RuntimeException("Workout Set not found"));
+        return workoutSet.orElseThrow(() -> new ResourceNotFoundException("Workout Set not found"));
     }
+
+    public WorkoutSet createWorkoutSet(WorkoutSet workoutSet) {
+        return workoutSetRepository.save(workoutSet);
+    }
+
+    public void deleteWorkoutSetById(Long id) {
+        if (!workoutSetRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        try {
+            workoutSetRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Workout Set cannot be deleted");
+        }
+    }
+
+    public void updateData (WorkoutSet entity, WorkoutSet obj) {
+        entity.setWorkout(obj.getWorkout());
+        entity.setSets(obj.getSets());
+        entity.setWeight(obj.getWeight());
+        entity.setReps(obj.getReps());
+        entity.setExercise(obj.getExercise());
+    }
+
+    public WorkoutSet update (Long id, WorkoutSet obj) {
+        try {
+            WorkoutSet entity = workoutSetRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return workoutSetRepository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
 }
