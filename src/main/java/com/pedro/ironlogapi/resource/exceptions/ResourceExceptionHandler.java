9406e31e596ee0327525608916c66310c3e6e1a8
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-20 16:14:08 -0300

    feat: implement global exception handling with standardized error responses

--- src/main/java/com/pedro/ironlogapi/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/pedro/ironlogapi/resource/exceptions/ResourceExceptionHandler.java
@@ -2,22 +2,46 @@ package com.pedro.ironlogapi.resource.exceptions;
 
 import com.pedro.ironlogapi.service.exceptions.DatabaseException;
 import com.pedro.ironlogapi.service.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> resourceNotFound(ResourceNotFoundException e){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler(DatabaseException.class)
-    public ResponseEntity<String> database(DatabaseException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        String error = "Unprocessable Entity";
+        HttpStatus status = HttpStatus.valueOf(422);
+
+        ValidationError err =  new ValidationError(Instant.now(), status.value(), error, "Erro de validação", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
     }
 
 }
---
+++ src/main/java/com/pedro/ironlogapi/resource/exceptions/StandardError.java
@@ -0,0 +1,21 @@
+package com.pedro.ironlogapi.resource.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class StandardError implements Serializable {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
