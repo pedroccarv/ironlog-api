89856a4a654aae53bfcfe2ff81b54e4ce6f047ba
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-21 19:41:43 -0300

    fix: add missing FieldMessage class for validation errors

---
+++ src/main/java/com/pedro/ironlogapi/resource/exceptions/FieldMessage.java
@@ -0,0 +1,19 @@
+package com.pedro.ironlogapi.resource.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class FieldMessage implements Serializable {
+
+    private String fieldName;
+    private String message;
+
+}
