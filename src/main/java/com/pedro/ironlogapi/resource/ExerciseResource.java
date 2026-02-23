25c6ae43b7973541e272aa504497b1d54961691b
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-23 13:43:42 -0300

    refactor: pluralize exercise endpoint to match REST conventions

--- src/main/java/com/pedro/ironlogapi/resource/ExerciseResource.java
+++ src/main/java/com/pedro/ironlogapi/resource/ExerciseResource.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/exercise")
+@RequestMapping(value = "/exercises")
 public class ExerciseResource {
 
     @Autowired
